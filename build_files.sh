cde62ab3994e359bef13eb6a28ea4e18508b85f1
james <jamesomare922@gmaill.com>
2024-05-30 07:25:14 +0300

    updated python version

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,10 @@
-# build_files.sh
-pip install -r requirements.txt
+# Build the project
+echo "Building the project..."
+python3.10 -m pip install -r requirements.txt
 
-# make migrations
-python3.9 manage.py migrate 
-# python3.9 manage.py collectstatic
+echo "Make Migration..."
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
+
+# echo "Collect Static..."
+# python3.10 manage.py collectstatic --noinput --clear
